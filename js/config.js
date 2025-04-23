8914cf6d09f7ae9269daf24fdee78753a192f3b1
BurntSuqar <13148754080@163.com>
2025-04-23 20:09:53 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,5 +4,6 @@ window.addEventListener('beforeunload', () => {
 });
 const DEEPSEEK_CONFIG  = {
   API_KEY: 'sk-92e5dd2058534d93b95c96c66cfb39ed',  // 在此处填写你的实际API密钥
-  API_URL: 'https://api.deepseek.com/v1/chat/completions' // DeepSeek官方API地址
+  API_URL: 'https://api.deepseek.com/v1/chat/completions', // DeepSeek官方API地址
+  API_TEXT: '\n用英语修正并只输出下面格式\n年份品牌车型' // 查询格式
 };
